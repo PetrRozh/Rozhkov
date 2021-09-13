dd7a2f082b1aeedbac7d87bcbf7d824321ce2621
PetrRozh <90225458+PetrRozh@users.noreply.github.com>
2021-09-14 03:54:53 +0500

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-a = [i+1 for i in range(50)]
+n=int(input('Введи кол-во чисел: '))
+a=[]
+for i in range(n):
+    a += list(map(int, input().split()))
 a.sort(reverse=True)
-for i in range(len(a)):
-    el1=a[i]
-    el2=a[i+1]
-    el3=a[i+2]
-    if el1+el2>el3 and el2+el3>el1 and el1+el3>el2:
-        p=(el1+el2+el3)//2
-        s=(p*(p-el1)*(p-el2)*(p-el3))**0.5
+f=0
+for i in range(n):
+    if a[i]<1:
+        f=+1
         break
-print(s,el1,el2,el3)
+    else: continue
+if f==0:
+    for i in range(len(a)):
+        el1=a[i]
+        el2=a[i+1]
+        el3=a[i+2]
+        if el1+el2>el3 and el2+el3>el1 and el1+el3>el2:
+            p=(el1+el2+el3)//2
+            s=(p*(p-el1)*(p-el2)*(p-el3))**0.5
+            break
+    print(s,el1,el2,el3)
